b05d54228f16538b64818ecc5e7c17a8ad1c49e1
Volha <volha@MacBook-Pro--Volha.local>
2023-01-17 11:17:24 +0100

    refactoring

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_svg/svg.dart';
-import 'package:profile_app/gen/assets.gen.dart';
-import 'package:profile_app/theme/colors_theme.dart';
-import 'package:profile_app/theme/app_text_theme.dart';
 import 'package:profile_app/widgets/avatar_widget.dart';
 import 'package:profile_app/widgets/name_widget.dart';
 import 'package:profile_app/widgets/select_type_widget.dart';
@@ -14,29 +9,7 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        elevation: 1.0,
-        backgroundColor: ColorsTheme.white,
-        leading: IconButton(
-          icon: SvgPicture.asset(Assets.icons.back.path),
-          onPressed: () {
-            //  TODO: realize button press
-          },
-        ),
-        title: const Text(
-          'Profile',
-          style: AppTextTheme.headline6,
-        ),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {
-              //  TODO: realize button press
-            },
-            icon: SvgPicture.asset(Assets.icons.menu.path),
-          ),
-        ],
-      ),
+      appBar: AppBar(),
       body: Column(
         children: const [
           AvatarWidget(),
---
+++ lib/widgets/app_bar_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:profile_app/gen/assets.gen.dart';
+import 'package:profile_app/theme/app_text_theme.dart';
+import 'package:profile_app/theme/colors_theme.dart';
+
+class AppBarWidget extends StatelessWidget {
+  const AppBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: false,
+      elevation: 1.0,
+      backgroundColor: ColorsTheme.white,
+      leading: IconButton(
+        icon: SvgPicture.asset(Assets.icons.back.path),
+        onPressed: () {
+          //  TODO: realize button press
+        },
+      ),
+      title: const Text(
+        'Profile',
+        style: AppTextTheme.headline6,
+      ),
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {
+            //  TODO: realize button press
+          },
+          icon: SvgPicture.asset(Assets.icons.menu.path),
+        ),
+      ],
+    );
+  }
+}
